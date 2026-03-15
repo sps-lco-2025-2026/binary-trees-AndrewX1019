5e8e8eca3e08716c7611f18633dbefbcf4ef0c45
AmdrewX1019 <xianga@stpaulsschool.org.uk>
2026-03-15 14:34:52 +0000

    /
    /

---
+++ Binary Tree Library/Class1.cs
@@ -0,0 +1,159 @@
+﻿using System.IO.Compression;
+
+namespace Binary_Tree_Library;
+
+public class Binary_Tree
+{
+    Node _root;
+    public int Count
+    {
+        get
+        {
+            if (_root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return _root.Count;
+            }
+        }
+    }
+    public Binary_Tree()
+    {
+        _root=null;
+    }
+    public Binary_Tree(int v)
+    {
+        _root=new Node(v);
+    }
+    public void Insert(int v)
+    {
+        if (_root == null)
+        {
+            _root=new Node(v);
+        }
+        else
+        {
+            _root.insert(v);
+        }
+    }
+    public bool Find(int v)
+    {
+        if (_root == null)
+        {
+            return false;
+        }
+        else
+        {
+            return _root.find(v);
+        }
+    }
+    public override string ToString()
+    {
+        if (_root == null)
+        {
+            return "None";
+        }
+        else
+        {
+            return _root.ToString();
+        }
+    }
+}
+
+public class Node
+{
+    int _value;
+    Node _Left;
+    Node _Right;
+    bool Exsistence;
+    internal Node(int v)
+    {
+        _value=v;
+    }
+    internal int Count
+    {
+        get
+        {
+            if (_Left == null)
+            {
+                if (_Right == null)
+                {
+                    return 1;
+                }
+                else return _Right.Count+1;
+            }
+            else
+            {
+                if(_Right == null)
+                {
+                    return _Left.Count+1;
+                }
+                return _Left.Count+_Right.Count+1;
+            }
+        }
+    }
+    internal void insert(int v)
+    {
+        if (v >= _value)
+        {
+            if (_Right == null)
+            {
+                _Right=new Node(v);
+                return;
+            }
+            _Right.insert(v);
+        }
+        else
+        {
+            if (_Left == null)
+            {
+                _Left=new Node(v);
+                return;
+            }
+            _Left.insert(v);
+        }
+    }
+    public bool find(int v)
+    {
+        if (_value == v)
+        {
+            return true;
+        }
+        if (v > _value&&_Right!=null)
+        {
+            return _Right.find(v);
+        }
+        if (v < _value && _Left != null)
+        {
+            return _Left.find(v);
+        }
+        return false;
+    }
+    public override string ToString()
+    {
+        if (_Left == null)
+        {
+            if (_Right == null)
+            {
+                return Convert.ToString(_value);
+            }
+            else
+            {
+                return Convert.ToString(_value)+" "+_Right.ToString();
+            }
+        }
+        else
+        {
+            if (_Right == null)
+            {
+                return _Left.ToString()+" "+Convert.ToString(_value);
+            }
+            else
+            {
+                return _Left.ToString()+" "+Convert.ToString(_value)+" "+_Right.ToString();
+            }
+        }
+    }
+}
---
+++ Binary Tree Test/Test1.cs
@@ -0,0 +1,34 @@
+﻿using Binary_Tree_Library;
+
+namespace Binary_Tree_Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Testcreate()
+    {
+        var b_t = new Binary_Tree(1);
+        var B_T = new Binary_Tree();
+    }
+    [TestMethod]
+    public void Testinsert()
+    {
+        var b_t = new Binary_Tree();
+        b_t.Insert(2);
+        b_t.Insert(0);
+        b_t.Insert(1);
+        b_t.Insert(3);
+        b_t.Insert(4);
+        Assert.AreEqual("0 1 2 3 4", b_t.ToString());
+    }
+    [TestMethod]
+    public void Testcount()
+    {
+        var b_t = new Binary_Tree();
+        b_t.Insert(3);
+        b_t.Insert(4);
+        b_t.Insert(2);
+        Assert.AreEqual(3, b_t.Count);
+    }
+}
